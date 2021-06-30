67f30aeddfa9156982f77f82ee86dd4ad5cecbeb
Bridget <bridgetshidok@gmail.com>
2021-06-30 17:12:35 +0100

    added the change capacity

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -2,15 +2,62 @@ const assertEquals= require("./../test-framework")
 const Basket= require("./../src/basket")
 const item= require("./../src/item")
 
-let input, result 
+let input, result, result2, result3, result4;
 //SETUP ADD FUNCITON
+console.log("can add one item");
 basket = new Basket()
 
 //EXECUTE ADD FUNCTION
 result=basket.addItem("bagel")
 
 //VERIFY ADD FUNCTION
-console.log("can add one item")
 console.log(assertEquals(result.length,1))
+
+//SETUP ADD FUNCITON
+console.log('')
+console.log("can add two items");
+basket = new Basket()
+
+//EXECUTE ADD FUNCTION
+result=basket.addItem("bagel")
+result2 = basket.addItem("bagel small");
+
+//VERIFY ADD FUNCTION
+console.log(assertEquals(result2.length, 2));
+
+
+//SETUP REMOVE FUNCITON
+console.log("")
+console.log("can remove one item");
+
+basket = new Basket()
+basket.addItem("bagel");
+
+//EXECUTE ADD FUNCTION
+result=basket.removeItem("bagel")
+
+//VERIFY ADD FUNCTION
+console.log(assertEquals(result.length,1))
+console.log(result)
+console.log(`this is basket ${basket}`);
+console.log(assertEquals(result.includes("bagel small"), false))
+console.log(assertEquals(result.includes("bagel"), false));
+
+//SETUP CHANGE_CAPACITY FUNCITON
+console.log("")
+console.log("can change capacity");
+
+basket = new Basket()
+result = basket.addItem("bagel");
+result2 = basket.addItem("bagel small");
+result3 = basket.addItem("bagel2");
+result4 = basket.addItem("bagel small2");
+
+//EXECUTE ADD FUNCTION
+result=basket.change_capacity(2)
+
+//VERIFY ADD FUNCTION
+console.log(assertEquals(result, 6))
+
 
 
--- src/basket.js
+++ src/basket.js
@@ -1,16 +1,46 @@
-class Basket {// class for basket
-    constructor(){
-        this._item= []
- //constructing array
-
+class Basket {
+    // class for basket
+    constructor() {
+        this._item = [];
+        this._capacity = this._capacity;
+        //constructing array
     }
-    addItem(items){// insert item to array
+    addItem(items) {
+        // insert item to array
 
-        this._item.push(items)
+        this._item.push(items);
         // returns array
         return this._item;
+    }
 
+    removeItem(items) {
+        //find the index of the item that wants to be deleted
+        const index = this._item.indexOf(items);
+        //Splice function takes in 3 parameters which are (index_of_item, How_many_item, what_item_)
+        this._item.splice(index, 1);
+
+        return this._item;
+    }
+
+    change_capacity(value) {
+        let length_basket = this._item.length;
+        this._capacity = 4;
+        console.log(length_basket);
+        console.log(this._capacity);
+        if (length_basket >= this._capacity) {
+            console.log(" the capacity is over the limit");
+            console.log(`we will increase capacity by ${value}`);
+            this._capacity += value;
+            console.log(this._capacity)
+        }
+        return this._capacity
     }
 }
-let basket = new Basket("bagel")
-module.exports= Basket;
+basket = new Basket();
+basket.addItem("bagels");
+basket.addItem("bagels small");
+basket.addItem("bagels 2");
+basket.addItem("bagels small 2");
+console.log(basket.change_capacity(2));
+
+module.exports = Basket;
